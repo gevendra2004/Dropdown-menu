faf0d5fad4bb6413733d4385de8146ead9946df0
Gevendra Sahu <gevendra2004@gmail.com>
2023-06-14 13:51:23 +0530

    css and js added

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const showDropdown = (content, button) =>{
+    const dropdownContent = document.getElementById(content),
+          dropdownButton = document.getElementById(button)
+ 
+    dropdownButton.addEventListener('click', () =>{
+       // We add the show-dropdown class, so that the menu is displayed
+       dropdownContent.classList.toggle('show-dropdown')
+    })
+ }
+ 
+ showDropdown('dropdown-content','button');
