1d4fdfe53b3c812db7a35a9a6cafb97df7088a41
Gevendra Sahu <gevendra2004@gmail.com>
2023-06-14 13:58:35 +0530

    Final CSS and JS

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-const showDropdown = (content, button) =>{
-    const dropdownContent = document.getElementById(content),
-          dropdownButton = document.getElementById(button)
- 
+const dropdownContent = document.getElementById('dropdown-content');
+const dropdownButton = document.getElementById('button');
+
+const showDropdown = () => {
     dropdownButton.addEventListener('click', () =>{
-       // We add the show-dropdown class, so that the menu is displayed
-       dropdownContent.classList.toggle('show-dropdown')
-    })
- }
- 
- showDropdown('dropdown-content','button');
+        // We add the show-dropdown class, so that the menu is displayed
+        dropdownContent.classList.toggle('show-dropdown')
+     })
+}
+
+showDropdown();
